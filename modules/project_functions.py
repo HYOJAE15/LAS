d1fea7a06f917fe86911f04ad68ead2919ca1215
dokyoon0712 <dkyoon0712@gmail.com>
2024-06-19 21:09:11 +0900

    [test]

--- modules/project_functions.py
+++ modules/project_functions.py
@@ -168,7 +168,7 @@ class ProjectFunctions(object):
         city_gt = os.path.join(path, "gtFine")
         os.makedirs(city_gt , exist_ok=True)
         
-        # test_please_0619
+        # test
         
         subfolders = ["train", "val", "test"]
         for subfolder in subfolders:
